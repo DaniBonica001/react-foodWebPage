c1ed536871c8829f1cfcf0fc63cd19dc552c6871
DaniBonica001 <danielabc_0106@hotmail.com>
2023-07-31 11:35:22 -0500

    Menu list

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,11 @@
+
+function Menu() {
+  return (
+    <div className="menu">
+        <h1 className="menuTitle">Our Menu</h1>
+        <div className="menuList"></div>
+    </div>
+  )
+}
+
+export default Menu
