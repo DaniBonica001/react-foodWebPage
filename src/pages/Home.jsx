61a522fe0b4e513744fb79eaee1e8e7ec5d0fd96
DaniBonica001 <danielabc_0106@hotmail.com>
2023-07-31 11:29:02 -0500

    fixed Home page to fit the image in the screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,8 @@ import PizzaBackground from '../assets/pizza.jpeg'
 
 function Home() {
     return (
-        <div className="home">
-            <div className="headerContainer" style={{ backgroundImage: `url(${PizzaBackground})` }}>
+        <div className="home" style={{ backgroundImage: `url(${PizzaBackground})` }}>
+            <div className="headerContainer" >
                 <h1>Pedro's Pizzeria</h1>
                 <p>PIZZA TO FIT ANY TASTE</p>
                 <Link to='/menu'>
