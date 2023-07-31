74fabe1b744dff9ec44dc64329b89d9651d26b07
DaniBonica001 <danielabc_0106@hotmail.com>
2023-07-31 11:19:50 -0500

    Basis home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import '../styles/Home.css'
+import PizzaBackground from '../assets/pizza.jpeg'
+
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className="home">
+            <div className="headerContainer" style={{ backgroundImage: `url(${PizzaBackground})` }}>
+                <h1>Pedro's Pizzeria</h1>
+                <p>PIZZA TO FIT ANY TASTE</p>
+                <Link to='/menu'>
+                    <button>
+                        ORDER NOW
+                    </button>
+                </Link>
+
+            </div>
+        </div>
+    )
 }
 
 export default Home
